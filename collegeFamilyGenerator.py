34e196097f3784feade0aa048da8481ba28f1bfe
AndrewTHowell <howelldrew99@gmail.com>
2019-08-22 16:48:54 +0100

    PEP8 Compliant

--- collegeFamilyGenerator.py
+++ collegeFamilyGenerator.py
@@ -1,55 +1,69 @@
-############### College Family Generator ###############
+
+###############################################################################
+# # # # # # # # # # # # # # College Family Generator # # # # # # # # # # # # #
+###############################################################################
 
 # Import Pandas library, used to store information about each child and parent
 import pandas as pd
 
-############### Importing CSV Files ###############
+###############################################################################
+# Importing CSV Files
+###############################################################################
 
 # Location of Folder containing CSV Files
-CSVLocation = "D:\howel\OneDrive - Durham University\Exec\VP Development\College Families\college-family-generator\Test Data"
+CSVLocation = ("D:\\howel\\OneDrive - Durham University\\Exec\\VP Development"
+               "\\College Families\\college-family-generator\\Test Data")
 
 # Create the Parents Panda, extracting info from CSV files
-parents = pd.read_csv("{0}\Parents.csv".format(CSVLocation), skiprows=1,
-            names=["email","name1","name2","name3","yearGoingInto","childrenAlready",
-                   "subjects","contactAmount","meetingPlaces",
-                   "arts","sports","entertainment","nightOut"],
-            usecols=range(1,14))
+parents = pd.read_csv("{0}\\Parents.csv".format(CSVLocation), skiprows=1,
+                      names=["email", "name1", "name2", "name3",
+                             "yearGoingInto", "childrenAlready", "subjects",
+                             "contactAmount", "meetingPlaces",
+                             "arts", "sports", "entertainment", "nightOut"],
+                      usecols=range(1, 14))
 
 # Create the Parents Panda, extracting info from CSV files
-children = pd.read_csv("{0}\Children.csv".format(CSVLocation), skiprows=1,
-            names=["email","name","subject",
-                   "contactAmount","meetingPlaces",
-                   "arts","sports","entertainment","nightOut"],
-            usecols=range(1,10))
+children = pd.read_csv("{0}\\Children.csv".format(CSVLocation), skiprows=1,
+                       names=["email", "name", "subject",
+                              "contactAmount", "meetingPlaces",
+                              "arts", "sports", "entertainment", "nightOut"],
+                       usecols=range(1, 10))
 
 # Add ID columns to both Pandas
-parents.insert(0, "ID", range(0,len(parents)))
-children.insert(0, "ID", range(0,len(children)))
+parents.insert(0, "ID", range(0, len(parents)))
+children.insert(0, "ID", range(0, len(children)))
 
-###################################################
+###############################################################################
 
-################# Pandas are setup and ready, structure shown below #################
+###############################################################################
+# Pandas are setup and ready, structure shown below
+###############################################################################
 
-# Parents : ID, email, name1, name2, name3, yearGoingInto, childrenAlready, subjects,
-#           contactAmount, meetingPlaces, arts, sports, entertainment, nightOut
+# Parents : ID, email, name1, name2, name3, yearGoingInto, childrenAlready,
+#           subjects, contactAmount, meetingPlaces, arts, sports, entertainment
+#           , nightOut
 
-# Children: ID, email, name, subject, contactAmount, meetingPlaces, arts, sports,
-#           entertainment, nightOut
+# Children: ID, email, name, subject, contactAmount, meetingPlaces, arts,
+#           sports, entertainment, nightOut
 
-#####################################################################################
+###############################################################################
 
 
+###############################################################################
+# Forming Allocation Data Structure
+###############################################################################
 
-############### Forming Allocation Data Structure ###############
-
-#row = parents.loc[0]
-#email = row["email"]
+# row = parents.loc[0]
+# email = row["email"]
 
 # Allocation stored in
 allocation = []
 
-#################################################################
+###############################################################################
 
 
+###############################################################################
+#
+###############################################################################
 
-###############  ###############
+###############################################################################
