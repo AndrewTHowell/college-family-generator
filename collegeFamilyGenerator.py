6a86f4d72a3d4f8be166a520d3aba9e8d48b4668
AndrewTHowell <howelldrew99@gmail.com>
2019-08-22 12:15:19 +0100

    Added Comments + CSVLocation
    CSVLocation -> variable set at the beginning, showing which flder the parents/children csv files are, variable placed in read_csv path

--- collegeFamilyGenerator.py
+++ collegeFamilyGenerator.py
@@ -1,18 +1,52 @@
 ### College Family Generator
 
+# Import Pandas library, used to store information about each child and parent
 import pandas as pd
 
-parents = pd.read_csv("Test Data\Parents.csv", skiprows=1,
-            names=["email","name1","name2","name3","yearGoingInto","childrenAlready","subjects",
-                   "contactAmount","meetingPlaces",
+
+############### Importing CSV Files ###############
+
+# Location of Folder containing CSV Files
+CSVLocation = "Test Data\"
+
+# Create the Parents Panda, extracting info from CSV files
+parents = pd.read_csv("{0}Parents.csv".format(CSVLocation), skiprows=1,
+            names=["email","name1","name2","name3","yearGoingInto","childrenAlready",
+                   "subjects","contactAmount","meetingPlaces",
                    "arts","sports","entertainment","nightOut"],
             usecols=range(1,14))
 
-children = pd.read_csv("Test Data\Children.csv", skiprows=1,
+# Create the Parents Panda, extracting info from CSV files
+children = pd.read_csv("{0}Children.csv".format(CSVLocation), skiprows=1,
             names=["email","name","subject",
                    "contactAmount","meetingPlaces",
                    "arts","sports","entertainment","nightOut"],
             usecols=range(1,10))
 
-print(parents.iloc[0]["email"])
+# Add ID columns to both Pandas
+parents.insert(0, "ID", range(0,len(parents)))
+children.insert(0, "ID", range(0,len(children)))
 
+###################################################
+
+
+
+################# Pandas are setup and ready, structure shown below #################
+
+# Parents : ID, email, name1, name2, name3, yearGoingInto, childrenAlready, subjects,
+#           contactAmount, meetingPlaces, arts, sports, entertainment, nightOut
+
+# Children: ID, email, name, subject, contactAmount, meetingPlaces, arts, sports,
+#           entertainment, nightOut
+
+#####################################################################################
+
+
+############### Forming Allocation Data Structure ###############
+
+
+#row = parents.loc[0]
+#email = row["email"]
+
+# Allocation stored in
+allocation = []
