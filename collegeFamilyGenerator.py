38481d0e54cb31df01a8318ba15f363322624429
AndrewTHowell <howelldrew99@gmail.com>
2019-08-26 13:37:35 +0100

    Sections and Regions Added

--- collegeFamilyGenerator.py
+++ collegeFamilyGenerator.py
@@ -1,14 +1,14 @@
 
-###############################################################################
 # # # # # # # # # # # # # # College Family Generator # # # # # # # # # # # # #
-###############################################################################
+
+# Section: Importing Modules
 
 # Import Pandas library, used to store information about each child and parent
 import pandas as pd
 
-###############################################################################
-# Importing CSV Files
-###############################################################################
+# Section End
+
+# Section: Importing CSV files
 
 # Location of Folder containing CSV Files
 CSVLocation = ("D:\\howel\\OneDrive - Durham University\\Exec\\VP Development"
@@ -33,11 +33,7 @@ children = pd.read_csv("{0}\\Children.csv".format(CSVLocation), skiprows=1,
 parents.insert(0, "ID", range(0, len(parents)))
 children.insert(0, "ID", range(0, len(children)))
 
-###############################################################################
-
-###############################################################################
-# Pandas are setup and ready, structure shown below
-###############################################################################
+# Region: Pandas are setup and ready, structure shown below
 
 # Parents : ID, email, name1, name2, name3, yearGoingInto, childrenAlready,
 #           subjects, contactAmount, meetingPlaces, arts, sports, entertainment
@@ -46,33 +42,41 @@ children.insert(0, "ID", range(0, len(children)))
 # Children: ID, email, name, subject, contactAmount, meetingPlaces, arts,
 #           sports, entertainment, nightOut
 
-###############################################################################
+# Region End
+
+# Section End
 
 
-###############################################################################
-# Forming Allocation Data Structure
-###############################################################################
+# Section: Forming Allocation Data Structure
 
 # row = parents.loc[0]
 # email = row["email"]
 
-# Allocation stored in 2D Array: [ParentID, ChildID]
+# Allocation stored in 2D Array
 allocation = []
 
 # Add Parents to allocation
-# Each Parent has 3 child 'slots'
+# For each parent in the panda
 for index, row in parents.iterrows():
+    # Each Parent has 3 child 'slots', so add 3 arrays
     for i in range(3):
         allocation.append([row["ID"], -1])
 
 for row in allocation:
     print(row)
 
-###############################################################################
+# Region: Allocation Array is setup and ready, structure shown below
 
+# Allocation = [ [ParentID, ChildID], [ParentID, ChildID], ... ]
 
-###############################################################################
-#
-###############################################################################
+# Where ParentID repeats 3 times (3 child slots)
+# [
+#   [0, -1], [0, -1], [0, -1],
+#   [1, -1], [1, -1], [1, -1],
+#   [2, -1], [2, -1], [2, -1],
+#     ...  ,   ...  ,   ...  ,
+# ]
 
-###############################################################################
+# Region End
+
+# Section End
